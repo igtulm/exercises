0a9736279bf2b0506987437b1cc59995bbacd207
Igor Tulmentyev <igtulm@gmail.com>
2018-09-17 13:07:28 +0300

    remove trash from header file

--- bin_search/c_binsearch/c_binsearch.h
+++ bin_search/c_binsearch/c_binsearch.h
@@ -2,14 +2,6 @@
 #define C_BINSEARCH_H
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-// OS X
-#ifdef __MACH__
-#include <mach/clock.h>
-#include <mach/mach.h>
-#endif
 
 int binsearch_middle_calc(int *arr, size_t size, int num);
 
