585063b2d8df16861e206de98dd8a67b813190fb
Igor Tulmentyev <igtulm@gmail.com>
2019-01-21 14:20:02 +0300

    refactoring hashmap

--- freqdict/include/hashmap.h
+++ freqdict/include/hashmap.h
@@ -11,7 +11,7 @@ using std::unordered_map;
 
 class HashMap {
  public:
-  void Insert(string s);
+  void Insert(string s) { storage_[s]++; }
   bool IsPresented(string s) { return isPresented(s); }
   unsigned GetFreq(string s) { return isPresented(s) ? storage_[s] : 0; }
 
@@ -20,16 +20,5 @@ class HashMap {
 
   bool isPresented(string s) { return storage_.find(s) != storage_.end(); }
 };
-
-void HashMap::Insert(string s) {
-  if (storage_.find(s) == storage_.end()) {
-    storage_[s] = 1;
-    return;
-  }
-
-  unsigned freq = storage_[s];
-  ++freq;
-  storage_[s] = freq;
-}
 
 }  // namespace freqdict
